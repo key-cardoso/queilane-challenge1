5829c56d70839c12528790181e8fae6bc3d7b559
key-cardoso <queilanecardoso7@gmail.com>
2023-03-27 22:46:00 -0300

    Inserindo validacao da tela6

--- js/validacao6.js
+++ js/validacao6.js
@@ -1,18 +1,27 @@
-function valid() {
-    var nome = card.nome;
-    var email = card.email;
-    var age = card.age;
-    if (nome.value == "") {
-        nome.focus();
-        alert("Prencha o campo nome")
-    }
-    if (email.value == "" || email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
-        email.focus();
-        alert("Prencha o campo email")
-    }
-    if (age.value == "") {
-        age.focus();
-        alert("Prencha o age")
+function validarForm() {
+    const nomeInput = document.getElementsByName('name')[0];
+    const emailInput = document.getElementsByName('email')[0];
+    const idadeInput = document.getElementsByName('age')[0];
 
+    if (!nomeInput.checkValidity()) {
+        alert('Por favor, insira seu nome completo.');
+        return false;
     }
+
+    if (!emailInput.checkValidity()) {
+        alert('Por favor, insira um endereço de e-mail válido.');
+        return false;
+    }
+
+    if (!idadeInput.checkValidity()) {
+        alert('Por favor, insira uma idade válida entre 18 e 60 anos.');
+        return false;
+    }
+
+    // Se tudo estiver certo, exibir mensagem de sucesso e redirecionar para a tela 1
+    alert('Formulário enviado com sucesso!');
+    window.location.href = 'tela1.html';
+
+    // Impedir que o formulário seja submetido
+    return false;
 }
